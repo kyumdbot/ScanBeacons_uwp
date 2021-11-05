cbb5190cefb4ad9e45e8531d6b3052ac9f2196b0
kyumdBot <kyumd001@gmail.com>
2021-11-05 16:10:18 +0800

    first commit

---
+++ ScanBeacons/BeaconInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace ScanBeacons
+{
+    public struct BeaconInfo
+    {
+        public Guid Uuid { get; }
+        public ushort Major { get; }
+        public ushort Minor { get; }
+        public sbyte TxPower { get; }
+        public short Rssi { get; }
+
+        public BeaconInfo(Guid uuid, ushort major, ushort minor, sbyte txPower, short rssi)
+        {
+            Uuid = uuid;
+            Major = major;
+            Minor = minor;
+            TxPower = txPower;
+            Rssi = rssi;
+        }
+
+        public static BeaconInfo Create(byte[] data, short rssi)
+        {
+            if (data[0] != 0x02)
+            {
+                throw new ArgumentException("First byte in array was exptected to be 0x02", "bytes");
+            }
+            if (data[1] != 0x15)
+            {
+                throw new ArgumentException("Second byte in array was expected to be 0x15", "bytes");
+            }
+            if (data.Length != 23)
+            {
+                throw new ArgumentException("Byte array length was expected to be 23", "bytes");
+            }
+
+            var uuid = new Guid(
+                        BitConverter.ToInt32(data.Skip(2).Take(4).Reverse().ToArray(), 0),
+                        BitConverter.ToInt16(data.Skip(6).Take(2).Reverse().ToArray(), 0),
+                        BitConverter.ToInt16(data.Skip(8).Take(2).Reverse().ToArray(), 0),
+                        data.Skip(10).Take(8).ToArray());
+
+            ushort major = BitConverter.ToUInt16(data.Skip(18).Take(2).Reverse().ToArray(), 0);
+            ushort minor = BitConverter.ToUInt16(data.Skip(20).Take(2).Reverse().ToArray(), 0);
+            sbyte txPower = (sbyte)data[22];
+
+            return new BeaconInfo(uuid, major, minor, txPower, rssi);
+        }
+
+        public override string ToString()
+        {
+            return $"Uuid: {Uuid}, Major: {Major}, Minor: {Minor}, TxPower: {TxPower}, Rssi: {Rssi}";
+        }
+    }
+}
---
+++ ScanBeacons/MainPage.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Storage.Streams;
+using Windows.System.Threading;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ScanBeacons
+{
+    public sealed partial class MainPage : Page
+    {
+        private BluetoothLEAdvertisementWatcher watcher;
+        private List<ulong> beaconIDs = new List<ulong>();
+        private ObservableCollection<BeaconInfo> Beacons = new ObservableCollection<BeaconInfo>();
+        private bool isScanning = false;
+
+        #region UI Code
+
+        public MainPage()
+        {
+            InitializeComponent();
+            CopiedPanel.Visibility = Visibility.Collapsed;
+            watcher = new BluetoothLEAdvertisementWatcher();
+            watcher.Received += OnAdvertisementReceived;
+            ChangeScanState();
+        }
+
+        private async void OnAdvertisementReceived(BluetoothLEAdvertisementWatcher watcher,
+                                                   BluetoothLEAdvertisementReceivedEventArgs eventArgs)
+        {
+            const ushort AppleCompanyId = 0x004C;
+            foreach (var adv in eventArgs.Advertisement.ManufacturerData.Where(x => x.CompanyId == AppleCompanyId))
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    Debug.WriteLine($"LocalNmae: {eventArgs.Advertisement.LocalName}");
+
+                    UpdateBeacons(adv.Data,
+                                  eventArgs.BluetoothAddress,
+                                  eventArgs.RawSignalStrengthInDBm);
+
+                    Debug.WriteLine($"> Count : {beaconIDs.Count}, {Beacons.Count}");
+                });
+            }
+        }
+
+        private void UpdateBeacons(IBuffer buffer, ulong address, short rssi)
+        {
+            byte[] bytes = new byte[buffer.Length];
+            using (var reader = DataReader.FromBuffer(buffer))
+            {
+                reader.ReadBytes(bytes);
+            }
+
+            if (bytes[0] != 0x02 || bytes[1] != 0x15 || bytes.Length != 23)
+            {
+                return;
+            }
+
+            var beaconInfo = BeaconInfo.Create(bytes, rssi);
+            Debug.WriteLine($"addr: {address}");
+            Debug.WriteLine($"beaconInfo: {beaconInfo}");
+
+            int index = beaconIDs.IndexOf(address);
+            Debug.WriteLine($"index: {index}");
+            if (index >= 0)
+            {
+                // update
+                Beacons[index] = beaconInfo;
+            }
+            else
+            {
+                // add
+                beaconIDs.Add(address);
+                Beacons.Add(beaconInfo);
+            }
+            Debug.WriteLine("-----------------");
+        }
+        private void ChangeScanState()
+        {
+            isScanning = !isScanning;
+            if (isScanning)
+            {
+                // clear
+                beaconIDs.Clear();
+                Beacons.Clear();
+
+                // start
+                watcher.Start();
+                ScanButton.Content = "Stop";
+                StateProgressBar.Visibility = Visibility.Visible;
+                Debug.WriteLine("Bluetooth LE Advertisement Watcher Started.");
+            }
+            else
+            {
+                watcher.Stop();
+                ScanButton.Content = "Scan";
+                StateProgressBar.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        #endregion
+
+        #region Click Events
+
+        private void ScanButton_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine(">> ScanButton_Click");
+            ChangeScanState();
+        }
+
+        private void BeaconListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            Debug.WriteLine(">> BeaconListView_ItemClick");
+            Debug.WriteLine($">> Item: {e.ClickedItem}");
+
+            var dataPackage = new DataPackage
+            {
+                RequestedOperation = DataPackageOperation.Copy
+            };
+            dataPackage.SetText(e.ClickedItem.ToString());
+            Clipboard.SetContent(dataPackage);
+
+            // show CopiedPanel
+            if (CopiedPanel.Visibility == Visibility.Collapsed)
+            {
+                CopiedPanel.Visibility = Visibility.Visible;
+                var delay = TimeSpan.FromSeconds(12.5);
+                var DelayTimer = ThreadPoolTimer.CreateTimer((source) =>
+                {
+                    _ = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                    {
+                        // hide CopiedPanel
+                        CopiedPanel.Visibility = Visibility.Collapsed;
+                    });
+                }, delay);
+            }
+        }
+
+        #endregion
+    }
+}
